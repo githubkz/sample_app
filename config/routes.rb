7ac60e0e5cd4c39771d97f35817c5532606f4877
kz kasai <rgb.cmyk.iroiro@gmail.com>
2015-01-02 03:02:48 +0000

    aa

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 SampleApp::Application.routes.draw do
+  get "static_pages/home"
+  root 'static_pages#home'
+  match "/help",  to:"static_pages#help", via: 'get'
+  get "static_pages/help"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
